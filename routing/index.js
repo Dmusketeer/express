c5168520be7da233f1f32779e4637c196c3fafcf
Dmusketeer <ardmountronics@gmail.com>
2021-07-11 00:06:39 +0530

    index js file

--- routing/index.js
+++ routing/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+var product = require('./routes/product')
+var users = require('./routes/user')
+var items = require('./routes/items')
 var app = express();
 var path = require('path');
 var port = 3000;
+
+
+app.use('/pro',product); //pro name must be same in the product.js file.
+app.use('/user',users); //u name must be same in the user.js file.
+// app.use('/item',items); //it name must be same in the items.js file.
+
+
+
+
+
 
 //home page:
 app.get('/', (req, res) => {
@@ -10,32 +23,32 @@ app.get('/', (req, res) => {
 
 
 // for user routes:
-app.get('/user', (req, res)=>{
-    res.send("get request for user");
-});
-app.post('/user-create', (req, res) => {
-    res.send("post request for user");
-});
-app.put('/user-update', (req, res) => {
-    res.send("put request for user");
-});
-app.delete('/user-delete', (req, res) => {
-    res.send("delete request for user");
-});
+// app.get('/user', (req, res)=>{
+//     res.send("get request for user");
+// });
+// app.post('/user-create', (req, res) => {
+//     res.send("post request for user");
+// });
+// app.put('/user-update', (req, res) => {
+//     res.send("put request for user");
+// });
+// app.delete('/user-delete', (req, res) => {
+//     res.send("delete request for user");
+// });
 
 // for product routes:
-app.get('/product', (req, res) => {
-    res.send("get request for product");
-});
-app.post('/product-create', (req, res) => {
-    res.send("post request for product");
-});
-app.put('/product-update', (req, res) => {
-    res.send("put request fot product");
-});
-app.delete('/product-delete', (req, res) => {
-    res.send("delete request for product");
-});
+// app.get('/product', (req, res) => {
+//     res.send("get request for product");
+// });
+// app.post('/product-create', (req, res) => {
+//     res.send("post request for product");
+// });
+// app.put('/product-update', (req, res) => {
+//     res.send("put request fot product");
+// });
+// app.delete('/product-delete', (req, res) => {
+//     res.send("delete request for product");
+// });
 
 
 app.listen(port,()=>{
